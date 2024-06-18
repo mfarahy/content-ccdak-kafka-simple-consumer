63d7c1fc0c9ffa828e42b2e579781e274e6c4fc7
mohammad_farahy <mfarahy@gmail.com>
2024-06-18 12:10:41 +0200

    .

--- src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
+++ src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
@@ -23,7 +23,7 @@ public class Main {
         config.put("group.id","test");
 
         try(KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(config)){
-            consumer.subscribe(Arrays.asList("inventory purchases"));
+            consumer.subscribe(Arrays.asList("inventory_purchases"));
 
             while(true){
                 ConsumerRecords<String,String> events=consumer.poll(Duration.ofSeconds(1));
@@ -32,7 +32,8 @@ public class Main {
                     return;
                 }
                 for(ConsumerRecord<String,String> event:events){
-                    System.out.format("%s\n", event.value());
+                    System.out.format("key: %s, value: %s, offset: %d, partition: %d \n",
+                            event.key(), event.value(), event.offset(), event.partition());
                 }
             }
         }
