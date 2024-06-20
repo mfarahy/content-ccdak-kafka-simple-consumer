2c605fb0afeb647a8abdd2c611e47cc87e46b94a
mohammad_farahy <mfarahy@gmail.com>
2024-06-20 10:55:57 +0200

    implement purchase stream

--- src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
+++ src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
@@ -1,40 +1,70 @@
 package com.linuxacademy.ccdak.kafkaSimpleConsumer;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 
-import java.time.Duration;
-import java.util.Arrays;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
+
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Properties config=new Properties();
+        final Properties properties = new Properties();
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "inventory-data");
+        properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
-        config.put("bootstrap.servers","localhost:9092");
-        config.put("enable.auto.commit","true");
-        config.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-        config.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-        config.put("auto.offset.reset","earliest");
-        config.put("allow.auto.create.topics","false");
-        config.put("group.id","test");
+        final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, String> source = builder
+                .stream("inventory_purchases");
 
-        try(KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(config)){
-            consumer.subscribe(Arrays.asList("inventory_purchases"));
+        final KStream<String, Integer> integerValueSource = source.mapValues(value -> {
+            try {
+                return Integer.valueOf(value);
+            } catch (NumberFormatException e) {
+                System.out.format("Unable to convert to integer: %s\n", value);
+                return 0;
+            }
+        });
 
-            while(true){
-                ConsumerRecords<String,String> events=consumer.poll(Duration.ofSeconds(1));
-                if (events.isEmpty()) {
-                    System.out.println("No events any more!");
-                    return;
+        final KTable<String, Integer> productCounts = integerValueSource
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.Integer()))
+                .reduce((total, newQuantity) -> total + newQuantity);
+
+        productCounts.toStream()
+                .to("total_purchases", Produced.with(Serdes.String(), Serdes.Integer()));
+
+       final Topology topology= builder.build();
+        try (KafkaStreams streams = new KafkaStreams(topology, properties)) {
+            System.out.println(topology.describe());
+
+            final CountDownLatch latch = new CountDownLatch(1);
+
+            Runtime.getRuntime().addShutdownHook(new Thread("shutdown-hook") {
+                @Override
+                public void run() {
+                    streams.close();
+                    latch.countDown();
                 }
-                for(ConsumerRecord<String,String> event:events){
-                    System.out.format("key: %s, value: %s, offset: %d, partition: %d \n",
-                            event.key(), event.value(), event.offset(), event.partition());
-                }
+            });
+
+            try {
+                streams.start();
+                latch.await();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+                System.exit(-1);
             }
         }
     }
