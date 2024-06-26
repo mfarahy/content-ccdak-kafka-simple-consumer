691877a48e8f0c9f95a5b43a2dcb12f4ebb6ae63
mohammad_farahy <mohammad_farahy@foxitsoftware.com>
2024-06-26 11:01:58 +0200

    read file

--- src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
+++ src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
@@ -1,13 +1,38 @@
 package com.linuxacademy.ccdak.kafkaSimpleConsumer;
 
-import java.util.concurrent.CountDownLatch;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hi Latch!");
+        if (args.length == 0) {
+            System.out.println("No file is provided!");
+            System.exit(-1);
+            return;
+        }
 
-            final CountDownLatch latch = new CountDownLatch(1);
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            System.out.println("File not found!");
+            System.exit(-1);
+            return;
+        }
+
+        Properties config=new Properties();
+        config.put("bootstrap.servers","localhost:9092");
+        config.put("")
+
+        try {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                reader.lines().forEach(System.out::println);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
