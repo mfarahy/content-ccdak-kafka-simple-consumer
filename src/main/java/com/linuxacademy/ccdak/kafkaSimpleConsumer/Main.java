a07f8bd5bbc1d1255b1f983677959e829e76f105
mohammad_farahy <mohammad_farahy@foxitsoftware.com>
2024-06-27 10:18:01 +0200

    add avro serializer

--- src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
+++ src/main/java/com/linuxacademy/ccdak/kafkaSimpleConsumer/Main.java
@@ -1,72 +1,19 @@
 package com.linuxacademy.ccdak.kafkaSimpleConsumer;
 
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.Duration;
-import java.util.Arrays;
+import org.apache.kafka.clients.producers.*;
 import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        if (args.length == 0) {
-            System.out.println("No file is provided!");
-            System.exit(-1);
-            return;
-        }
-
-        File file = new File(args[0]);
-        if (file.exists()) {
-            System.out.println("File already exists!");
-            System.exit(-1);
-            return;
-        }
-
         Properties config = new Properties();
-        config.put("bootstrap.servers", "localhost:9092");
+        config.put("ProducerConfig", "localhost:9092");
         config.put("key.deserializer", org.apache.kafka.common.serialization.StringDeserializer.class.getName());
         config.put("value.deserializer", org.apache.kafka.common.serialization.StringDeserializer.class.getName());
-        config.put("client.id", "topic-to-file");
-        config.put("group.id", "topic-to-file");
-        config.put("auto.offset.reset", "earliest");
-        config.put("enable.auto.commit", "false");
+        config.put("client.id", "schema-registry-lab");
         config.put("allow.auto.create.topics", "false");
 
-        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(config)) {
-            consumer.subscribe(Arrays.asList("inventory_purchases"));
-
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-                while (true) {
-                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(5));
-                    if (records.isEmpty()) {
-                        System.out.println("No event remains, going to end");
-                        break;
-                    }
-
-                    records.forEach(record -> {
-                        try {
-                            writer.write(String.format("key=%s, value=%s, topic=%s, partition=%d, offset=%d\n",
-                                    record.key(), record.value(), record.topic(), record.partition(), record.offset()));
-
-                            System.out.print(".");
-                        } catch (IOException e) {
-                            System.out.println(e.getMessage());
-                            System.exit(-1);
-                        }
-                    });
-                    writer.flush();
-                    consumer.commitAsync();
-                }
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-                System.exit(-1);
-            }
-        }
+        KafkaProducer<String, Purchase> consumer = new KafkaProducer<>(config);
     }
 }
